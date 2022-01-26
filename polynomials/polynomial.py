173804cca9a172761c63bbaffc8b2166d3be120d
qc420 <qc420@imperial.ac.uk>
2022-01-26 12:27:59 +0000

    Addition and equality

--- polynomials/polynomial.py
+++ polynomials/polynomial.py
@@ -1,3 +1,5 @@
+from numbers import Number
+
 class Polynomial: #don't need (object) as this is python2 requirement
     def __init__(self,coefs):
         """set attribute"""
@@ -30,3 +32,34 @@ class Polynomial: #don't need (object) as this is python2 requirement
 
         # Sum polynomial terms from high to low exponent.
         return " + ".join(reversed(terms)) or "0"
+
+    def __eq__(self, other):
+
+        return self.coefficients == other.coefficients
+
+    def __add__(self,other): 
+        #we can just add the coefficients straight because tuple addition
+        #is concatenate, we need to loop over the tuple and add them together
+        if isinstance (other, Polynomial):#this only works when self and other are poly
+            #first we need to work out the minimal common degree of both
+            common = min(self.degree(), other.degree()) +1 #e.g. x^3, y^2 return 2+1=3
+
+            coefs = tuple(a+b for a,b in zip(self.coefficients,other.coefficients))
+            #zip will stop when either of them run out
+            #now we need to deal with coefs in the longer poly that is not in the other poly
+            coefs += self.coefficients[common:] + other.coefficients[common:]
+            #now coefs is complete
+
+            return Polynomial(coefs) #return a new object with type class
+
+        elif isinstance(other,Number):
+            return Polynomial( (self.coefficients[0]+other,)+
+                                self.coefficients[1:])
+
+        else:
+            return NotImplemented
+
+    def __radd__(self, other):
+        return self + other  #it is going to call the __add__ method above
+                            
+
